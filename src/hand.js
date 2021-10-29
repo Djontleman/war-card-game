a2954c36d653d4110695d48614077dc2624d6c89
Djontleman <jhutchinson531@gmail.com>
2021-10-29 19:03:18 +0100

    Add addCardToHand function and removeCardFromHand function

--- src/hand.js
+++ src/hand.js
@@ -2,8 +2,16 @@
 
 const Card = require("../card.js");
 
-const Hand = function(card = null) {
-    this.card = card;
+const Hand = function() {
+    this.card = [];
+}
+
+Hand.prototype.addCardToHand = function(card) {
+    this.card.push(card);
+}
+
+Hand.prototype.removeCardFromHand = function() {
+    return this.card.pop();
 }
 
 module.exports = Hand;
