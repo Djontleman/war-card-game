57975278ed0481b4f3b2404cf0feed33143b3e61
Djontleman <jhutchinson531@gmail.com>
2021-10-29 22:14:27 +0100

    Make Deck class

---
+++ src/deck.js
@@ -0,0 +1,16 @@
+// ====== Deck Class ====== //
+
+const Deck = function() {
+    this.deck = [];
+    this.numberOfCards = this.deck.length;
+}
+
+Deck.prototype.addCardToBottomOfDeck = function(card) {
+    this.deck.unshift(card);
+}
+
+Deck.prototype.removeCardFromTopOfDeck = function() {
+    this.deck.pop();
+}
+
+module.exports = Deck;
