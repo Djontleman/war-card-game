2198d6a8b8cb1c809271ce7bfe1cf9131d3c5dca
Djontleman <jhutchinson531@gmail.com>
2021-10-29 23:18:34 +0100

    Make Stage class without War functionality

---
+++ src/stage.js
@@ -0,0 +1,19 @@
+// ====== Stage Class ====== //
+
+const Deck = require("./deck");
+
+const Stage = function() {
+    this.warPile = new Deck;
+}
+
+Stage.prototype.compareCards = function(card1, card2) {
+    if (card1 >= card2) {
+        return card1;
+    } else {
+        return card2;
+    }
+}
+
+Stage.prototype.war = function() {}
+
+module.exports = Stage;
