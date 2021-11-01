8e7673fa36d9c6aff55d03055d19a70eb6f4078f
Djontleman <jhutchinson531@gmail.com>
2021-11-01 23:20:32 +0000

    Attempt to fix infinite loop with startState/currentState

--- src/game.js
+++ src/game.js
@@ -27,11 +27,18 @@ Game.prototype.start = function() {
 
     this.dealCards();
 
+    // const startState = [];
+    // for (card of this.player1.deck.cards) {
+    //     startState.push(card.value);
+    // }
+    // console.log(startState);
+    // return;
+
     console.log();
     console.log("Game begins!");
     console.log();
 
-    // i = 100;
+    // i = 20;
     do {
         // console.log(this.player1.deck);
         // console.log(this.player2.deck);
@@ -49,15 +56,31 @@ Game.prototype.start = function() {
         console.log("Player " + this.player2Number + "'s deck: " + this.player2.deck.cards.length);
 
         console.log();
+
+        // const currentState = [];
+        // for (i = 0; i < currentState.length; i++) {
+        //     currentState.pop();
+        // }
+        // for (card of this.player1.deck.cards) {
+        // currentState.push(card.value);
+        // }
+        // console.log(startState);
+        // console.log(currentState);
+
+        if (currentState == startState) {
+            console.log("An infinite loop has been reached!")
+            console.log("The game ends in a draw...")
+            return;
+        }
         
         // i--;
         // if (i == 0) {
-        //     break;
+        //     return;
         // }
 
     } while (this.winner == null);
 
-    console.log("The game winner is player " + this.winner.playerNumber);  
+    console.log("The game winner is player " + this.winner.playerNumber + "!");  
 }
 
 Game.prototype.dealCards = function() {
